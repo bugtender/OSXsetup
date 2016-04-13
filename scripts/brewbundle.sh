20b43d53fa266ebb3e3e07a1cfa73e18f045c885
Vincent Lin <bugtender@users.noreply.github.com>
2016-04-13 22:09:56 +0800

    Update brewbundle.sh
    Update brewbundle.sh

--- scripts/brewbundle.sh
+++ scripts/brewbundle.sh
@@ -1,8 +1,10 @@
 # Brew Bundle list
 # Please make sure brew is installed.
 
-bash tap.sh
-bash git.sh
-bash essential.sh
-bash editor.sh
-bash develop.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+bash ${DIR}/tap.sh
+bash ${DIR}/git.sh
+bash ${DIR}/essential.sh
+bash ${DIR}/editor.sh
+bash ${DIR}/develop.sh
